472ce3743ca5c8c7282d017b30eecb60a80e5b1a
Daniel Santiago <daniel.santiagoti@gmail.com>
2021-01-17 12:26:02 -0300

    Primeiro commit

---
+++ src/com/poo/Produto.java
@@ -0,0 +1,34 @@
+package com.poo;
+
+import java.math.BigDecimal;
+
+public class Produto {
+
+    private Long id;
+    private String nomeProduto;
+    private BigDecimal valorProduto;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public BigDecimal getValorProduto() {
+        return valorProduto;
+    }
+
+    public void setValorProduto(BigDecimal valorProduto) {
+        this.valorProduto = valorProduto;
+    }
+}
---
+++ src/com/poo/Venda.java
@@ -0,0 +1,59 @@
+package com.poo;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Venda {
+
+    private Long id;
+    private String descricao;
+    private String nomeCliente;
+    private  String enderecoEntrega;
+    private BigDecimal valorTotal;
+
+    private List<Produto> listaProdutos = new ArrayList<Produto>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getEnderecoEntrega() {
+        return enderecoEntrega;
+    }
+
+    public void setEnderecoEntrega(String enderecoEntrega) {
+        this.enderecoEntrega = enderecoEntrega;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+
+
+}
